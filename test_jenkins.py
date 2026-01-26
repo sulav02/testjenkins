c971effc9908e91a95621aa2f5b5896868034f96
sulav02 <sulav1208@gmail.com>
2026-01-26 12:12:05 +1100

    :white_check_mark: Testing of jenkins - Passed Successfully

---
+++ test_jenkins.py
@@ -0,0 +1,5 @@
+print("Hello, DevOps world!")
+print("Name: Sulav01")
+print("Passionate about Cloud, CI/CD, and Automation")
+print("Learning Python, Jenkins, Docker, and Kubernetes")
+print("This is my first public Python script!")
